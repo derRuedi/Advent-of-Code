2d163d7287686b1445bed9f2c7cf43d782d91b70
Rüdiger Foos <ruedier.foos@gmail.com>
2022-12-01 17:01:25 +0100

    simplify day1

--- 2022/day1/day1.py
+++ 2022/day1/day1.py
@@ -3,36 +3,11 @@
     https://adventofcode.com/2022/day/1
 '''
 
-# quick solution
-
-# # input from website
-# with open(f'input.txt', 'r') as f:
-#     data = f.read().splitlines()
-
-# elves = []
-# elve = []
-
-# for line in data:
-#     if line != "":
-#         elve.append(line)
-#     else:
-#         elves.append(elve)
-#         elve = []
-
-# elves = [sum(map(int, e)) for e in elves]
-
-# elves.sort(reverse=True)
-# print(elves[0])
-# print(sum(elves[0:3]))
-
-
-# ---
-# more pythonic solution
-
 # input from website
 with open('input.txt', 'r') as f:
     data = f.read()
 
-elves = [sum(map(int, e.split("\n"))) for e in data.split("\n\n")]
+elves = [sum(map(int, e.splitlines())) for e in data.split("\n\n")]
 elves.sort(reverse=True)
+
 print(elves[0], sum(elves[0:3]))
