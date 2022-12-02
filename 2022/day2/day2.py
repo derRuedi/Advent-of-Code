acd8ca078b5100a62dbb408e2b0b398b58a53ab7
Rüdiger Foos <ruedier.foos@gmail.com>
2022-12-02 13:13:46 +0100

    remove wet code

--- 2022/day2/day2.py
+++ 2022/day2/day2.py
@@ -3,23 +3,27 @@
     https://adventofcode.com/2022/day/2
 '''
 
+from itertools import product
+
 # input from website
 with open(f'input.txt', 'r') as f:
     data = [tuple(a.split(" ")) for a in f.read().splitlines()]
 
-# A or X for Rock
-# B or Y for Paper
-# C or Z for Scissors
-winners = [
-    ('A', 'Y'),
-    ('B', 'Z'),
-    ('C', 'X')
-]
-draws = [
-    ('A', 'X'),
-    ('B', 'Y'),
-    ('C', 'Z')
-]
+# A for Rock
+# B for Paper
+# C for Scissors
+winners = {
+    ('A', 'B'),  # paper beats rock
+    ('B', 'C'),  # scissors cut paper
+    ('C', 'A')  # rock breaks scissors
+}
+draws = {
+    ('A', 'A'),
+    ('B', 'B'),
+    ('C', 'C')
+}
+defeats = set(product(["A", "B", "C"], repeat=2)) - winners - draws
+
 
 # 0 if you lost, 3 if the round was a draw, 6 if you won
 score_lost, score_draw, score_won = 0, 3, 6
@@ -27,15 +31,14 @@ score_lost, score_draw, score_won = 0, 3, 6
 # 1 for Rock, 2 for Paper, 3 for Scissors
 points = {
     "A": 1,
-    "X": 1,
     "B": 2,
-    "Y": 2,
-    "C": 3,
-    "Z": 3,
+    "C": 3
 }
 
 score = 0
 for game in data:
+    # substitute X, Y, Z for A, B, C, respectively
+    game = (game[0], "A" if game[1] == "X" else "B" if game[1] == "Y" else "C")
     if game in winners:
         score += score_won + points[game[1]]
     elif game in draws:
@@ -47,23 +50,11 @@ print(score)
 # part 2
 score = 0
 for game in data:
-    if game[1] == "X":  # X means you need to lose
-        score += score_lost
-        if game[0] == "A":
-            score += points["Z"]
-        elif game[0] == "B":
-            score += points["X"]
-        else:
-            score += points["Y"]
-    elif game[1] == "Y":  # Y means you need to end the round in a draw
-        add = score_draw + points[game[0]]
-        score += add
-    else:  # Z means you need to win
-        score += score_won
-        if game[0] == "A":
-            score += points["Y"]
-        elif game[0] == "B":
-            score += points["Z"]
-        else:
-            score += points["X"]
+    if game[1] == "X":  # lose
+        score += score_lost + \
+            points[[b for a, b in defeats if a == game[0]][0]]
+    elif game[1] == "Y":  # draw
+        score += score_draw + points[game[0]]
+    else:  # win
+        score += score_won + points[[b for a, b in winners if a == game[0]][0]]
 print(score)
