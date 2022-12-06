74d45e1268429785da9f111718af3c137e4e33c8
Rüdiger Foos <ruedier.foos@gmail.com>
2022-12-06 08:59:50 +0100

    optimize day6

--- 2022/day6/day6.py
+++ 2022/day6/day6.py
@@ -3,19 +3,17 @@
     https://adventofcode.com/2022/day/6
 '''
 
-from itertools import permutations
-
 # input from website
 with open(f'input.txt', 'r') as f:
     data = f.read()
 
 
-def find_marker(marker_length=4):
+def find_marker(marker_length):
     for i in range(0, len(data) - marker_length):
-        if all(a[0] != a[1] for a in permutations(data[i:i+marker_length], 2)):
+        if len(set(data[i:i+marker_length])) == marker_length:
             print(i+marker_length)
             break
 
 
-find_marker()
-find_marker(14)
+for x in (4, 14):
+    find_marker(x)
