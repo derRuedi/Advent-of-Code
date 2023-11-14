109850be7f6221a5fdaad279d86aea2f89bd9011
Rüdiger Foos <ruedier.foos@gmail.com>
2023-11-14 11:38:43 +0100

    add modifications for template creation

--- create_day.sh
+++ create_day.sh
@@ -11,7 +11,7 @@ TEMPLATE=`cat <<-EOM
 
 # input from website
 sample_input = False
-input = 'day{day}_sample_input.txt' if sample_input else 'day{day}.txt'
+input = 'day{day}_sample.txt' if sample_input else 'day{day}.txt'
 with open(input, 'r') as f:
     data = f.read().splitlines()
 
@@ -48,14 +48,16 @@ else
   # create the python file
   echo "$TEMPLATE" | sed -e "s/{day}/${day}/g" -e "s/{year}/${year}/g" > "day${day}.py"
   
+  # create the description file - unfortunately it still has to be filled manually
+  touch "day${day}_description.txt"
   # create the input data file so that it exists no matter what
   touch "day${day}.txt"
   # if possible, get the data
   aocd "$day" "$year" > "day${day}.txt"
   # create the description file so that it exists no matter what
-  touch "day${day}_description.txt"
+  touch "day${day}_sample.txt"
   # if possible, get the data
-  aocd "$day" "$year" --example-parser simple >> "day${day}_description.txt"
+  aocd "$day" "$year" --example-parser simple >> "day${day}_sample.txt"
   # open everything in VS Code
   code *
 fi
